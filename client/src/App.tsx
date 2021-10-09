86d3afea1fb0adc7d712f237f727ce99dbb438e5
NaririAreg <hnairiareg@gmail.com>
2021-10-10 00:36:12 +0400

    Setting everything

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,14 @@ import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <h1>CRUD Application</h1>
 
-      <div className="d-flex flex-column align-items-center">
-        <input type="text" name="movieName" />
-        <input type="text" name="review" />
+      <div className="d-flex flex-column align-items-center justify-content-center">
+        <input type="text" name="movieName" className='form-control w-50' placeholder='Movie Name' />
+        <input type="text" name="review" className='form-control w-50' placeholder='Review' />
+
+        <button className='btn btn-success mt-3'>Submit</button>
       </div>
     </div>
   )
